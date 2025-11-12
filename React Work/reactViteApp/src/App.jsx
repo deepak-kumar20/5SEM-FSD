34fef9c95ff5aa078e384f1fb24a9daf630bdf31
Deepak Kumar <deepakdagur0007@gmail.com>
2025-11-12 09:13:53 +0530

    routing done

--- React Work/reactViteApp/src/App.jsx
+++ React Work/reactViteApp/src/App.jsx
@@ -5,7 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Gallery from "./components/Gallery";
 import ImageManipulation from "./components/ImageManipulation";
 import StateHandling from "./components/StateHandling";
+import Dashboard from "./pages/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Registration from "./pages/Registration";
+import MainLayout from "./pages/MainLayout";
 function App() {
   return (
     <>
@@ -21,6 +24,9 @@ function App() {
           <Route path="/state" element={<StateHandling />} />
           <Route path="/image" element={<ImageManipulation />} /> */}
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<MainLayout/>}/>
         </Routes>
       </BrowserRouter>
 
