bda25a17f087090141b6ad280c4a9cf99f14a273
Deepak Kumar <deepakdagur0007@gmail.com>
2025-11-26 10:29:03 +0530

    add to cart section

--- React Work/reactViteApp/src/pages/Dashboard.jsx
+++ React Work/reactViteApp/src/pages/Dashboard.jsx
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
-  const [count, setCount] = useState(0)
-  const [point ,setPoint] = useState(10)
+  const [products, setProducts] = useState([]);
+  
   useEffect(() => {
-    console.log(count)
-    console.log(point)
-  },[count,point])
-  return (
-      <div>
-          <h1>
-        <button onClick={() => setCount(count + 1)}> click me</button>
-        <br />
-        <button onClick={() => setPoint(point + 10)}> add points</button>
-        <br />
-        Count: {count}
-        <br />
-        Points: {point}
-          </h1>
+    async function fetchData() {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+      
+    }
+    fetchData();
+  }, [])
+  return products.length === 0 ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="productContainer">
+      {products.map((item) => {
+        return (
+          <div
+            key={item.id}
+            style={{
+              border: "1px solid black",
+              margin: "10px",
+              padding: "10px",
+              width: "300px",
+            }}
+          >
+            {item.title}
+            <br />
+            <img
+              style={{
+                height: "100px",
+                width: "100px",
+              }}
+              src={item.image}
+              alt={item.title}
+            />
+            <p>{"Price: " + item.price}</p>
+            <p>{item.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default Dashboard
