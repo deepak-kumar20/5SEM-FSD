e05e0bc72bb8205b448e59660f911ce51d0d5b8c
Deepak Kumar <deepakdagur0007@gmail.com>
2025-11-26 09:57:51 +0530

    use Effect

--- React Work/reactViteApp/src/pages/Dashboard.jsx
+++ React Work/reactViteApp/src/pages/Dashboard.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
+  const [count, setCount] = useState(0)
+  const [point ,setPoint] = useState(10)
+  useEffect(() => {
+    console.log(count)
+    console.log(point)
+  },[count,point])
   return (
       <div>
           <h1>
-              Welcome to Dashboard
+        <button onClick={() => setCount(count + 1)}> click me</button>
+        <br />
+        <button onClick={() => setPoint(point + 10)}> add points</button>
+        <br />
+        Count: {count}
+        <br />
+        Points: {point}
           </h1>
     </div>
   )
